4cc1ef5b60968dffd320f2e594aa3a8ee8592556
chonlatit <chonlatit@thinknet.co.th>
2018-03-18 01:20:52 +0700

    starter

--- Starter/src/components/FormPin.js
+++ Starter/src/components/FormPin.js
@@ -2,51 +2,42 @@ import React, { Component } from 'react'
 
 class FormPin extends Component {
   state = {
-    addPoint: false,
+    pinLocation: false,
     createdBy: '',
     namePlace: '',
   }
 
   render() {
-    const { lng, lat } = this.props
-    const { createdBy, namePlace, addPoint } = this.state
     return (
       <div className='form-layout' >
-        {!addPoint ?
-          <div style={{ marginTop: 20 }} >
-            <button
-              className='bt-add-point'
-            >
-              เพิ่มจุดของเรา
-          </button>
+
+        <div className='from-add-marker' >
+          <div>
+            <div className="label" >Your name</div>
+            <input type='text' />
           </div>
-
-          :
-
-          <div className='from-add-marker' >
-            <div>
-              <div className="label" >Your name</div>
-              <input type='text' value={createdBy} onChange={this.changecreatedBy} />
-            </div>
-            <div>
-              <div className="label" >Name place</div>
-              <input type='text' value={namePlace} onChange={this.changeNamePlace} />
-            </div>
-            <div>
-              <div className="label" >lng , lat</div>
-              <input type='text' value={`${lng}, ${lat}`} readOnly />
-            </div>
-            <br />
-            <div>
-              <button
-                disabled={createdBy && namePlace ? false : true}
-              >
-                เพิ่มจุด
-              </button>
-            </div>
+          <div>
+            <div className="label" >Name place</div>
+            <input type='text' />
           </div>
+          <div>
+            <div className="label" >lng , lat</div>
+            <input type='text' readOnly />
+          </div>
+          <br />
+          <div>
 
-        }
+            {
+              this.state.pinLocation ?
+                <button> เพิ่มจุด </button>
+                :
+                <button className='bt-add-point'>
+                  เพิ่มจุดของเรา
+                </button>
+            }
+
+          </div>
+        </div>
       </div>
     )
   }
--- Starter/src/components/Map.js
+++ Starter/src/components/Map.js
@@ -1,63 +1,7 @@
 import React, { Component } from 'react'
-import mapmagic from 'mapmagic-gl'
 import '../styles/map.css'
-import '../../node_modules/mapmagic-gl/dist/mapmagic-gl.css'
-
-let map
-let pins = [
-  {
-    lng: 98.97,
-    lat: 18.80,
-    namePlace: 'วัดเจ็ดยอด',
-    createdBy: 'ชลธิต',
-  },
-  {
-    lng: 98.92,
-    lat: 18.8047,
-    namePlace: 'บ้านฉันเอง',
-    createdBy: 'ชลธิต'
-  }
-]
 
 class Map extends Component {
-
-  state = {
-    lng: 98.98,
-    lat: 18.79,
-    pins: pins,
-  }
-
-  componentDidMount = () => {
-    this.renderMap()
-  }
-
-  renderMap = () => {
-    const { pins } = this.state
-    map = new mapmagic.Map({
-      container: 'map',
-      app_id: 'example-m82la',
-      api_key: 'Yjk4YTliYzRmMTVlN2Y4ODhlYzRjZDE5OTU0NmViNjk',
-      center: [98.98, 18.79],
-      zoom: 12,
-    })
-    map.on('load', () => {
-      pins.forEach(obj => {
-        map.addMarker({
-          lng: obj.lng,
-          lat: obj.lat,
-          description: `
-              <strong>${obj.namePlace}</strong> 
-              <p>โดย: ${obj.createdBy}</p>
-            `,
-          icon: 'mmg_pin_1_orange',
-          popup: {
-            action: 'hover',
-            offset: [0, -20],
-          }
-        })
-      })
-    })
-  }
 
   render() {
     return (
